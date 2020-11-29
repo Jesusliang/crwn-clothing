d34f54ea2659e53c8f451bb50d056b058759fd75
jesusliang <jesusliang96@gmail.com>
2020-11-29 15:55:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import HomePage from "./homepage.component";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
